bd5a506b73b3feb95bb297fbda56138a61f4b5a5
afifarn <afifr0837@gmail.com>
2019-10-10 19:28:20 +0700

    first commit

---
+++ src/main/java/PBOUlang_10116138_Latihan27_HurufBesarKecil/HurufBesarKecil.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBOUlang_10116138_Latihan27_HurufBesarKecil;
+import java.util.Scanner;
+/**
+ *
+ * @author 
+ * NAMA     : Afif Raditya Nugraha
+ * KELAS    : PBO_ULANG/IF4
+ * NIM      : 10116138
+ * LOG      : Program ini berisi program untuk Menampilkan Huruf Besar Kecil
+ * NORunning
+ */
+public class HurufBesarKecil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Kalimat;
+        System.out.print("Masukan Kalimat : ");
+        Scanner scanner = new Scanner(System.in);
+        Kalimat = scanner.nextLine();
+        
+        String Besar = Kalimat.toUpperCase();
+        String Kecil = Kalimat.toLowerCase();
+        System.out.println();
+        System.out.println("========HasilFormating=======");
+        System.out.println("Huruf besar = " + Besar);
+        System.out.println("Huruf Kecil = " + Kecil);
+    }
+    
+}
